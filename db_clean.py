4a81c17cdf47ee0d130ebb991c91bae0233a7d52
David DiGiovanni <davedg629@gmail.com>
2017-02-15 22:22:39 -0500

    Fix db_clean.py script to delete tweets older than 60 days

--- db_clean.py
+++ db_clean.py
@@ -1,12 +1,16 @@
 from app import db
 from app.models import Tweet
+import time
 
+
+cutoff_dats = 60
+now = time.time()
+time_60_ago = now - 60 * 60 * 24 * 60
 
 tweets = Tweet.query\
-    .filter_by(tweeted=False)\
-    .filter(Tweet.date_posted < 1476876302)\
+    .filter(Tweet.date_posted < time_60_ago)\
     .all()
 
 for tweet in tweets:
-    tweet.tweeted = True
+    db.session.delete(tweet)
     db.session.commit()
